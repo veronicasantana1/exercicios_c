004b4f946436d3a52acbdd10510a09284059486f
Verônica Santana <137583641+veronicasantana1@users.noreply.github.com>
2024-02-03 20:55:05 -0300

    first commit

---
+++ 001.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Älo Mundo";
+    return 0;
+}
---
+++ chat.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Declaração de variável para armazenar a entrada do usuário
+    int numero;
+
+    // Solicita ao usuário para inserir um número
+    cout << "Digite um número: ";
+    
+    // Lê o número inserido pelo usuário
+    cin >> numero;
+
+    // Exibe o número de volta na tela
+    cout << "Você digitou: " << numero << endl;
+
+    return 0;
+}
